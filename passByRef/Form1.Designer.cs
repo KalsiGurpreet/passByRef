41f24bf00ad205c9ec2f2aee96db2f19f5b6b080
19023 <gups_kalsi@outlook.com>
2023-09-22 20:58:50 -0300

    Add project files.

---
+++ passByRef/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace passByRef
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputListBox = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputListBox
+            // 
+            this.outputListBox.FormattingEnabled = true;
+            this.outputListBox.ItemHeight = 25;
+            this.outputListBox.Location = new System.Drawing.Point(180, 80);
+            this.outputListBox.Name = "outputListBox";
+            this.outputListBox.Size = new System.Drawing.Size(343, 204);
+            this.outputListBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "&Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(389, 330);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(134, 59);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "&Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.outputListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox outputListBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ passByRef/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace passByRef
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // a variable must be initialized before passing
+            // an argument by reference using "reference" parameter variable.
+
+            int num1 = 98;
+            int num2 = 99;
+            int num3 = 100;
+
+
+            outputListBox.Items.Clear();
+
+            outputListBox.Items.Add("number1 is set to " + num1);
+            outputListBox.Items.Add("number2 is set to " + num2);
+            outputListBox.Items.Add("number3 is set to " + num3);
+
+
+            // control goes to SetToZero method with 
+            // value being passed by reference.
+
+            // in pass by reference, parameter variable is reference to the argument, it means 
+            // when argument is passed as a reference its value is changed instead of parameter variable.
+
+            // a keyword "ref" has been added to identify it as argument been passed by reference.
+
+            SetToZero(ref num1);
+            SetToZero(ref num2);
+            SetToZero(ref num3);
+
+            outputListBox.Items.Add("==========================");
+            outputListBox.Items.Add("number1 is set to " + num1);
+            outputListBox.Items.Add("number2 is set to " + num2);
+            outputListBox.Items.Add("number3 is set to " + num3);
+        }
+        private void SetToZero(ref int number) // number par variable references to num1 variable.
+        {
+            number = 0;
+            
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
